2608a7e1b2e451a83ec6af6babb2e1b885bc0a3a
mathanmaveric <mathanm@maveric-systems.com>
2023-11-08 19:04:03 +0530

    revert spring config props for intiall deployment

--- src/main/java/org/banker/loan/exception/NoDataExceptionMapper.java
+++ src/main/java/org/banker/loan/exception/NoDataExceptionMapper.java
@@ -3,14 +3,15 @@ package org.banker.loan.exception;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
+@Provider
 public class NoDataExceptionMapper implements ExceptionMapper<NoDataException> {
     @Override
     public Response toResponse(NoDataException exception) {
         return  Response.status(Response.Status.NOT_FOUND)
                 .entity(exception.getMessage())
                 .status(204)
-                .type(MediaType.TEXT_PLAIN)
                 .build();
     }
 }
